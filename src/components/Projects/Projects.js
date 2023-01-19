dbc964cfce5f9d8e8124b5bddb8dd137ebf50f41
Liam <liam.s.smith@gmail.com>
2023-01-19 15:45:49 -0500

    fix projects link in nav

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,9 +7,14 @@ import {
   Img,
   InfoColumn,
   TextWrapper,
-  SubTitle
+  SubTitle,
 } from "../InfoSection/InfoSection.elements";
-import { ProjectSection, ProjectInfoRow, Project, ProjectTitle } from "./Projects.elements";
+import {
+  ProjectSection,
+  ProjectInfoRow,
+  Project,
+  ProjectTitle,
+} from "./Projects.elements";
 
 const Projects = ({
   img,
@@ -21,54 +26,55 @@ const Projects = ({
   description2,
   description3,
   description4,
+  id,
 }) => {
   return (
     <ProjectSection>
-      <Container>
-      <Fade>
-        <TextWrapper>
-        <Heading>{headline}</Heading>
-        </TextWrapper>
-        <ProjectInfoRow>
-          <InfoColumn>
-            <Project>
-              <ProjectTitle>Tool Swap</ProjectTitle>
-              <ImageWrapper>
-                <Img src={img}/>
-              </ImageWrapper>
-              <SubTitle>{description}</SubTitle>
-            </Project>
-          </InfoColumn>
-          <InfoColumn>
-            <Project>
-            <ProjectTitle>Scheduler</ProjectTitle>
-              <ImageWrapper>
-              <Img src={img2}/>
-              </ImageWrapper>
-              <SubTitle>{description2}</SubTitle>
-            </Project>
-          </InfoColumn>
-        </ProjectInfoRow>
-        <ProjectInfoRow>
-          <InfoColumn>
-            <Project>
-              <ProjectTitle>Tweeter</ProjectTitle>
-              <ImageWrapper>
-              <Img src={img3}/>
-              </ImageWrapper>
-              <SubTitle>{description3}</SubTitle>
-            </Project>
-          </InfoColumn>
-          <InfoColumn>
-            <Project>
-              <ProjectTitle>Tiny App</ProjectTitle>
-              <ImageWrapper>
-              <Img src={img4}/>
-              </ImageWrapper>
-              <SubTitle>{description4}</SubTitle>
-            </Project>
-          </InfoColumn>
-        </ProjectInfoRow>
+      <Container id={id}>
+        <Fade>
+          <TextWrapper>
+            <Heading>{headline}</Heading>
+          </TextWrapper>
+          <ProjectInfoRow>
+            <InfoColumn>
+              <Project>
+                <ProjectTitle>Tool Swap</ProjectTitle>
+                <ImageWrapper>
+                  <Img src={img} />
+                </ImageWrapper>
+                <SubTitle>{description}</SubTitle>
+              </Project>
+            </InfoColumn>
+            <InfoColumn>
+              <Project>
+                <ProjectTitle>Scheduler</ProjectTitle>
+                <ImageWrapper>
+                  <Img src={img2} />
+                </ImageWrapper>
+                <SubTitle>{description2}</SubTitle>
+              </Project>
+            </InfoColumn>
+          </ProjectInfoRow>
+          <ProjectInfoRow>
+            <InfoColumn>
+              <Project>
+                <ProjectTitle>Tweeter</ProjectTitle>
+                <ImageWrapper>
+                  <Img src={img3} />
+                </ImageWrapper>
+                <SubTitle>{description3}</SubTitle>
+              </Project>
+            </InfoColumn>
+            <InfoColumn>
+              <Project>
+                <ProjectTitle>Tiny App</ProjectTitle>
+                <ImageWrapper>
+                  <Img src={img4} />
+                </ImageWrapper>
+                <SubTitle>{description4}</SubTitle>
+              </Project>
+            </InfoColumn>
+          </ProjectInfoRow>
         </Fade>
       </Container>
     </ProjectSection>
