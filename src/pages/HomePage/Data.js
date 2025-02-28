3c64c50450d4ab93000b3ace8a5d9e263c668e67
Liam <liam.s.smith@gmail.com>
2025-02-28 13:33:06 -0500

    minor update to content

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -58,7 +58,7 @@ export const homeObjFour = {
   lightTextDesc: false,
   headline: "About Me",
   description:
-    "As a developer and designer, I have created everything from single page sites to large, organizational web portals. I have a unique eye for design, a keen curiosity that drives me to understand problems and find solutions.",
+    "As a developer and designer with a background in the arts, I have created everything from single page sites to large, organizational web portals. I have a unique eye for design, a keen curiosity that drives me to understand problems and find solutions.",
   buttonLabel: "",
   img: require("../../images/Fairy-Lake.jpeg"),
   start: "",
